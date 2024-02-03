aa02c65ef3681175d76f0604ab24e19c5d48829c
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-02-03 17:51:37 +0530

    feat : added category

--- Finance_Automation.py
+++ Finance_Automation.py
@@ -4,52 +4,101 @@ file = f"budget.csv"
 
 transactions = []
 
-totalDebitedAmout = 0.0
-
-totalCreditedAmout = 0.0
-
-categoryTotalAmount = 0.0
-
 def finance(file):
         
     with open(file , mode = 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
-        global totalDebitedAmout
 
-        global categoryTotalAmount
+        global transactions
 
-        global totalCreditedAmout
+        
 
         for row in csv_reader:
             date = row[0]
             reason = row[1]
-            creditamount = float(row[2])
-            debitamount = float(row[3])
+            creditamount = int(row[2])
+            debitamount = int(row[3])
 
-            # totalDebitedAmout += debitamount
-
-            # totalCreditedAmout += creditamount
+            reason = row[1].lower()
 
             transaction = ((date,reason,creditamount,debitamount))
 
             transactions.append(transaction)
 
-            totalCreditedAmout += creditamount
+        return transactions
+    
+def debit_and_credit(file):
+    with open(file , mode = 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
+        totalDebitedAmout = 0
+
+        totalCreditedAmout = 0
+
+        for row in csv_reader:
+            creditamount = int(row[2])
+            debitamount = int(row[3])
             totalDebitedAmout += debitamount
 
-        finance = [transactions, totalDebitedAmout , totalCreditedAmout]
+            totalCreditedAmout += creditamount
 
-        return finance
+        return [totalCreditedAmout,totalDebitedAmout]
+
+    
+def category(file , categoryName = "credited"):
+    with open(file , mode = 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        categoryTotalAmount = 0
+
+        for row in csv_reader:
+            reason = row[1]
+            debitamount = int(row[3])
+
+            if reason == categoryName:
+                categoryTotalAmount += debitamount
         
-print("Transaction History:",finance(file)[0])
+        return categoryTotalAmount
 
-print("Debit History:",finance(file)[1])
 
-print("Credit History:",finance(file)[2])
+        
+# print("Transaction History:",finance(file))
+
+# print("Credit History:",debit_and_credit(file)[0])
+
+# print("Debit History:",debit_and_credit(file)[1])
+
+# print("Rent : " , category(file, "rent"))
 
    
+def main():
+    print("1 - Transaction History")
+    print("2 - Total Amout Credit")
+    print("3 -  Total Amout Debited")
+    print("4 - Total amount spent on particular reason")
 
-        
-        
+    option = int(input("Enter your Option"))
+
+
+
+    match option:
+        case 1:
+            print("Transaction History:",finance(file))
+
+        case 2:
+            print("Credit History:",debit_and_credit(file)[0])
+
+        case 3:
+            print("Debit History:",debit_and_credit(file)[1])
+    
+        case 4:
+            categoryName = input("Enter the category")
+            result = category(file, categoryName.lower())  # Assuming `category` is a function
+            print(categoryName , " is : ", result)
+
+    to_continue =input("Would You like to continue yes or no ")
+
+    if(to_continue.lower() == "yes"): main()    
+
+main()
